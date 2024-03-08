9cd9a62baac84a40899337480b2bbd1ca6f1a617
indusRealEstate <webtech@indusre.ae>
2024-03-08 12:55:02 +0400

    fix: :zap: fixed banners images loading issue.

--- src/components/property/property-single-style/single-v4/banners-gallery/GalleryBox.js
+++ src/components/property/property-single-style/single-v4/banners-gallery/GalleryBox.js
@@ -9,110 +9,7 @@ import "swiper/swiper-bundle.min.css";
 const GalleryBox = ({ banners, loading, size, openCommonDialog }) => {
   const skeletonLoader = size.width < 500 ? [1] : [1, 2, 3];
 
-  const [firstImageLoaded, setFirstImageLoaded] = useState(false);
-  const [secondImageLoaded, setSecondImageLoaded] = useState(false);
-  const [thirdImageLoaded, setThirdImageLoaded] = useState(false);
-  const [fourthImageLoaded, setFourthImageLoaded] = useState(false);
-  const [fifthImageLoaded, setFifthImageLoaded] = useState(false);
-  const [sixthImageLoaded, setSixthImageLoaded] = useState(false);
-  const [seventhImageLoaded, setSeventhImageLoaded] = useState(false);
-  const [eighthImageLoaded, setEighthImageLoaded] = useState(false);
-  const [ninthImageLoaded, setNinthImageLoaded] = useState(false);
-  const [tenthImageLoaded, setTenthImageLoaded] = useState(false);
-  const [eleventhImageLoaded, setEleventhImageLoaded] = useState(false);
-  const [twelvethImageLoaded, setTwelvethImageLoaded] = useState(false);
-  const [thirteenthImageLoaded, setThirteenthImageLoaded] = useState(false);
-  const [fourteenthImageLoaded, setFourteenthImageLoaded] = useState(false);
-  const [fifteenthImageLoaded, setFifteenthImageLoaded] = useState(false);
-
-  const getImgLoadVar = (index) => {
-    switch (index) {
-      case 0:
-        return firstImageLoaded;
-      case 1:
-        return secondImageLoaded;
-      case 2:
-        return thirdImageLoaded;
-      case 3:
-        return fourthImageLoaded;
-      case 4:
-        return fifthImageLoaded;
-      case 5:
-        return sixthImageLoaded;
-      case 6:
-        return seventhImageLoaded;
-      case 7:
-        return eighthImageLoaded;
-      case 8:
-        return ninthImageLoaded;
-      case 9:
-        return tenthImageLoaded;
-      case 10:
-        return eleventhImageLoaded;
-      case 11:
-        return twelvethImageLoaded;
-      case 12:
-        return thirteenthImageLoaded;
-      case 13:
-        return fourteenthImageLoaded;
-      case 14:
-        return fifteenthImageLoaded;
-      default:
-        break;
-    }
-  };
-
-  const getImgLoadFunc = (index) => {
-    switch (index) {
-      case 0:
-        setFirstImageLoaded(true);
-        break;
-      case 1:
-        setSecondImageLoaded(true);
-        break;
-      case 2:
-        setThirdImageLoaded(true);
-        break;
-      case 3:
-        setFourthImageLoaded(true);
-        break;
-      case 4:
-        setFifthImageLoaded(true);
-        break;
-      case 5:
-        setSixthImageLoaded(true);
-        break;
-      case 6:
-        setSeventhImageLoaded(true);
-        break;
-      case 7:
-        setEighthImageLoaded(true);
-        break;
-      case 8:
-        setNinthImageLoaded(true);
-        break;
-      case 9:
-        setTenthImageLoaded(true);
-        break;
-      case 10:
-        setEleventhImageLoaded(true);
-        break;
-      case 11:
-        setTwelvethImageLoaded(true);
-        break;
-      case 12:
-        setThirteenthImageLoaded(true);
-        break;
-      case 13:
-        setFourteenthImageLoaded(true);
-        break;
-      case 14:
-        setFifteenthImageLoaded(true);
-        break;
-      default:
-        break;
-    }
-  };
+  const [imagesLoaded, setImagesLoaded] = useState([]);
 
   return (
     <>
@@ -155,7 +52,7 @@ const GalleryBox = ({ banners, loading, size, openCommonDialog }) => {
                 </p>
 
                 {/* <a href={bnr.link} target="_blank"> */}
-                {!getImgLoadVar(index) ? (
+                {!imagesLoaded.includes(index) ? (
                   <Skeleton
                     // className="w-100 h-100"
                     variant="rectangular"
@@ -170,13 +67,15 @@ const GalleryBox = ({ banners, loading, size, openCommonDialog }) => {
                   width={size.width > 500 ? 480 : 360}
                   height={size.width > 500 ? 342 : 256}
                   className={`${
-                    !getImgLoadVar(index)
+                    !imagesLoaded.includes(index)
                       ? "opacity-0 position-absolute bdrs12 w-100"
                       : "opacity-100 bdrs12 w-100 position-relative"
                   }}`}
                   src={bnr.image}
                   alt={`Image ${index + 1}`}
-                  onLoadingComplete={() => getImgLoadFunc(index)}
+                  onLoadingComplete={() => {
+                    setImagesLoaded((prev) => [...prev, index]);
+                  }}
                 />
                 {/* </a> */}
               </div>
